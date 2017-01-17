6c5d024d0df0f0ccfa8eebd0cdcca6e68d82d960
Chiheb Hmida <chiheb.hmida@gmail.com>
2017-01-17 17:13:21 +0100

    un nouveau fichier

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8" />
+
+        <title>Ma page web</title>
+
+    </head>
+
+    <body>
+<?php /* on ne va rien afficher */ ?>
+        <h1>Ma page web</h1>
+
+        <p<?php /* on ne va rien afficher */ ?>>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+
+    </body>
+
+</html>
